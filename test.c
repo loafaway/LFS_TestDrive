1e881fe543f814e8a2c3c15e5412f2653de21417
五叔 <49177910+fifthuncle@users.noreply.github.com>
2023-03-23 09:43:49 +0800

    Update test.c
    Done by 'fifthuncle', a collaborator.

--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[]) {
 
 	for (int i = 0; i < argc; ++i)
-		printf("Argument%3d:  %s\n", i, argv);
+		printf("Argument%3d:  %s\n", i, argv[i]);
 
-	returnn 0;
+	return 0;
 }
